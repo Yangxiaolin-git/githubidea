9af49b1fa0800e01214babe3ae5ed5f3e1a4337c
Yangxiaolin <1669769092@qq.com>
2020-09-26 17:10:37 +0800

    hellogit dev

--- src/main/java/com/atguigu/bean/git.java
+++ src/main/java/com/atguigu/bean/git.java
@@ -7,6 +7,6 @@ package com.atguigu.bean;
 public class git {
     public static void main(String[] args) {
         System.out.println("今天是什么日子");
-        System.out.println("第一天见面的日子");
+        System.out.println("第一天见面的日子。。。");
     }
 }
